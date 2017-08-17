a2d60f3f2b38fe969dab59f09b367320989c4a1d
Angela Gathu and Ida Apayo <ida@>
2017-08-17 11:28:12 +0300

    third commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document) ready(function)(){
+         var person1Input = $("input#person1").val();
+         var person2Input = $("input#person2").val();
+         var animalInput= $("input#animal").val();
+         var exclamationInput = $("input#exclamation").val();
+         var verbInput = $("input#verb").val();
+         var nounInput = $("input#noun").val();
+
+  var car = $["person1Input","person2Input","animalInput","exclamationInput","verbInput","nounInput"]
+
+}
