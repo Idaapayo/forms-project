61eb216fee1b08146fac959b3b9b1ef48f6caba3
Angela Gathu and Ida Apayo <ida@>
2017-08-17 12:25:19 +0300

    sixth commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
   $("#car form").submit(function(event) {
-         var car1Input = $("input#person1").val();
-         console.log(car1Input);
-         var car2Input = $("input#person2").val();
-         var car3Input= $("input#animal").val();
-         var car4Input = $("input#exclamation").val();
-         var car5Input = $("input#verb").val();
-         var car6Input = $("input#noun").val();
+         var car1Input = $("input#car1").val();
+         var car2Input = $("input#car2").val();
+         var car3Input= $("input#car3").val();
+         var car4Input = $("input#car4").val();
+         var car5Input = $("input#car5").val();
+         var car6Input = $("input#car6").val();
 
-  var car = [car1, car2, car3, car4, car5, car6]
+         event.preventDefault();
+  var car = [car1Input, car2Input, car3Input, car4Input, car5Input, car6Input]
   console.log(car);
-  event.preventdefault();
   });
 });
